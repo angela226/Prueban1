8afd0ad23415517970b00d1c6e03c97b92dff174
Angela Maria Garcia <angelamarga264@gmail.com>
2024-07-08 08:32:21 -0500

    Se hizo el siguiente cambio 8:32am

--- PruebaAngela22.php
+++ PruebaAngela22.php
@@ -1,3 +1,3 @@
 <?php
-    echo "Hola, soy Angela! Ingenieria de Sistemas.  Contenido modificado 8 Jul 2024 8:27 AM";
+    echo "Hola, soy Angela! Ingenieria de Sistemas.  Contenido modificado 8 Jul 2024 8:31 AM";
 ?>
